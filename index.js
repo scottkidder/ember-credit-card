0687f9cdf371dd5e77632d50f6c0295a622aa5ac
Scott Kidder <scott.kidder@ilq.com>
2018-11-20 11:07:44 -0500

    feat: fastboot support

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 const path = require('path');
 const Funnel = require('broccoli-funnel');
 const MergeTrees = require('broccoli-merge-trees');
+/* eslint-disable-next-line node/no-extraneous-require */
+const map = require('broccoli-stew').map;
 
 module.exports = {
   name: 'ember-credit-card',
@@ -19,11 +21,15 @@ module.exports = {
       trees.push(vendorTree);
     }
 
-    trees.push(
-      new Funnel(path.join(this.project.root, 'node_modules', 'card', 'dist'), {
-        files: ['card.js'],
-      })
-    );
+
+    let cardVendor = new Funnel(path.join(this.project.root, 'node_modules', 'card', 'dist'), {
+      files: ['card.js'],
+    });
+
+    cardVendor = map(cardVendor, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
+
+
+    trees.push(cardVendor);
 
     return new MergeTrees(trees);
   },
